9e94b1e3866d0987403f257d7616d43ba9d2bb8d
温世波 <cthinker@126.com>
2017-04-13 10:05:39 +0800

    天气链接新增微店链接

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ class wechatCallbackapiTest
                     $result = $this->receiveText($postObj);
                     break;
                 case "image":
-                    $result = $this->receiveText($postObj);
+                    $result = $this->receiveImage($postObj);
                     break;
                 case "voice":
                     $result = $this->receiveText($postObj);
@@ -95,16 +95,30 @@ class wechatCallbackapiTest
         return $result;
     }
 
+    private function checkstr($str, $substr){
+        $needle = $substr;
+        $tmparray = explode($needle,$str);
+        if(count($tmparray) > 1){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
     private function receiveText($object)
     {
         $keyword = trim($object->Content);
         
-        $url = "http://api.map.baidu.com/telematics/v3/weather?output=json&ak=XohzuurALBUGvfFA6sGaLGjp&location=".urlencode($keyword);
+        $result = '';
 
-        $output = file_get_contents($url);
-        $content = json_decode($output, true);
-        
-        $result = $this->transmitNews($object, $content);
+        if (strpos($keyword, '失业') !== false) {
+            $result = $this->transmitWork($object);
+        } elseif (strpos($keyword, '工作') !== false) {
+            $result = $this->transmitWork($object);
+        } else {
+            $result = $this->transmitWeather($object);
+        }
+
         return $result;
     }
 
@@ -124,8 +138,46 @@ class wechatCallbackapiTest
         return $result;
     }
 
-    private function transmitNews($object, $newsArray)
+    private function transmitWork($object)
     {
+        $itemTpl = "<item>
+                    <Title><![CDATA[%s]]></Title>
+                    <Description><![CDATA[%s]]></Description>
+                    <PicUrl><![CDATA[%s]]></PicUrl>
+                    <Url><![CDATA[%s]]></Url>
+                    </item>";
+                    
+        $item_str = "";
+                        
+        $item_str .= sprintf($itemTpl, 
+            '你可以到这里找工作', 
+            '不客气，祝你好运', 
+            'http://7xqhcq.com1.z0.glb.clouddn.com/zjlife/1455784621-2029397591_n.png',  
+            'https://luna.58.com/list.shtml?plat=m&city=sh&cate=job&-15=20&utm_source=link&spm=u-Lt2pHoBa1luDubj.mzp_qzzp_qb_hshportal_zgz01');      
+        $newsTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[news]]></MsgType>
+            <Content><![CDATA[]]></Content>
+            <ArticleCount>%s</ArticleCount>
+            <Articles>
+            $item_str</Articles>
+            </xml>";
+
+        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count(1));
+        return $result;
+    }
+
+    private function transmitWeather($object)
+    {
+        $keyword = trim($object->Content);
+        
+        $url = "http://api.map.baidu.com/telematics/v3/weather?output=json&ak=XohzuurALBUGvfFA6sGaLGjp&location=".urlencode($keyword);
+
+        $output = file_get_contents($url);
+        $newsArray = json_decode($output, true);
+
         if(!is_array($newsArray)){
             return "";
         }
@@ -140,32 +192,32 @@ class wechatCallbackapiTest
         
         if($error != 0) {
             
-        $itemTpl = "    <item>
-        <Title><![CDATA[%s]]></Title>
-        <Description><![CDATA[%s]]></Description>
-        <PicUrl><![CDATA[%s]]></PicUrl>
-        <Url><![CDATA[%s]]></Url>
-    </item>
-";
-        
-        $item_str = "";
+            $itemTpl = "    <item>
+            <Title><![CDATA[%s]]></Title>
+            <Description><![CDATA[%s]]></Description>
+            <PicUrl><![CDATA[%s]]></PicUrl>
+            <Url><![CDATA[%s]]></Url>
+        </item>
+    ";
             
-        $item_str .= sprintf($itemTpl, 'Hi！我是至简生活，我把地球翻个遍儿也没找到 '.$keyword, '请输入正常的城市名',
-                                     'https://mmbiz.qlogo.cn/mmbiz/icdZ5NYAvaOvKVyZQIEuoYia5ptreV6Wnhe8zT0lYzeEInJEHSLqYQzf4Cw9LBqrCeWH84lfYHEqobYKjtKjC8rw/0?wx_fmt=jpeg', 
-                                     'http://mp.weixin.qq.com/s?__biz=MzA5MjE1NjI1OA==&mid=210526708&idx=1&sn=9b46af73e86fc1789d4ce393054034d7#rd');      
-        $newsTpl = "<xml>
-<ToUserName><![CDATA[%s]]></ToUserName>
-<FromUserName><![CDATA[%s]]></FromUserName>
-<CreateTime>%s</CreateTime>
-<MsgType><![CDATA[news]]></MsgType>
-<Content><![CDATA[]]></Content>
-<ArticleCount>%s</ArticleCount>
-<Articles>
-$item_str</Articles>
-</xml>";
+            $item_str = "";
+                
+            $item_str .= sprintf($itemTpl, 'Hi！我是至简生活，我把地球翻个遍儿也没找到 '.$keyword, '请输入正常的城市名',
+                                         'https://mmbiz.qlogo.cn/mmbiz/icdZ5NYAvaOvKVyZQIEuoYia5ptreV6Wnhe8zT0lYzeEInJEHSLqYQzf4Cw9LBqrCeWH84lfYHEqobYKjtKjC8rw/0?wx_fmt=jpeg', 
+                                         'https://weidian.com/s/1202372938?wfr=c&ifr=shopdetail');      
+            $newsTpl = "<xml>
+    <ToUserName><![CDATA[%s]]></ToUserName>
+    <FromUserName><![CDATA[%s]]></FromUserName>
+    <CreateTime>%s</CreateTime>
+    <MsgType><![CDATA[news]]></MsgType>
+    <Content><![CDATA[]]></Content>
+    <ArticleCount>%s</ArticleCount>
+    <Articles>
+    $item_str</Articles>
+    </xml>";
 
-        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count(1));
-        return $result;
+            $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count(1));
+            return $result;
         }
         
         
@@ -173,6 +225,8 @@ $item_str</Articles>
         
         $pm25 = $newsArray[results][0][pm25];
         
+        $index_data = $newsArray[results][0][index];
+
         $weather_data = $newsArray[results][0][weather_data];
         
         $itemTpl = "    <item>
@@ -190,18 +244,49 @@ $item_str</Articles>
             $item = $weather_data[$i];
             
             if($i == 0){
+                // http://life.html5.qq.com/
                 $item_str .= sprintf($itemTpl, $currentCity.' '.$item['date'].' '.$item['temperature'].' '.$item['weather'].' '.$item['wind'], 
                                      $item['date'].' '.$item['temperature'].' '.$item['weather'].' '.$item['wind'],
                                      'https://mmbiz.qlogo.cn/mmbiz/icdZ5NYAvaOvKVyZQIEuoYia5ptreV6Wnhe8zT0lYzeEInJEHSLqYQzf4Cw9LBqrCeWH84lfYHEqobYKjtKjC8rw/0?wx_fmt=jpeg', 
-                                     'http://mp.weixin.qq.com/s?__biz=MzA5MjE1NjI1OA==&mid=210526708&idx=1&sn=9b46af73e86fc1789d4ce393054034d7#rd');
+                                     'https://weidian.com/s/1202372938?wfr=c&ifr=shopdetail');
             }else {
                 $item_str .= sprintf($itemTpl, $item['date'].' '.$item['temperature'].' '.$item['weather'].' '.$item['wind'], 
                                  $item['date'].' '.$item['temperature'].' '.$item['weather'].' '.$item['wind'], 
                                  $this->dayOrNight() ? $item['dayPictureUrl'] : $item['nightPictureUrl'], 
-                                 'http://mp.weixin.qq.com/s?__biz=MzA5MjE1NjI1OA==&mid=210526708&idx=1&sn=9b46af73e86fc1789d4ce393054034d7#rd');
+                                 'https://weather.html5.qq.com/');
             }
 
         } 
+
+        for ($i = 0; $i < count($index_data); $i++) { 
+            $item = $index_data[$i];
+
+            if ($i == 0) {
+                // 穿衣
+                $item_str .= sprintf($itemTpl, $item['title'].' '.$item['des'], 
+                                 $item['title'].' '.$item['zs'].' '.$item['des'], 
+                                 '', 
+                                 'http://m.mogujie.com/');
+            } elseif ($i == 1) {
+                // 洗车
+                $item_str .= sprintf($itemTpl, $item['title'].' '.$item['des'], 
+                                 $item['title'].' '.$item['zs'].' '.$item['des'], 
+                                 '', 
+                                 'http://m.autohome.com.cn/');
+            } elseif ($i == 2) {
+                // 旅游
+                
+            } elseif ($i == 3) {
+                // 感冒
+                
+            } elseif ($i == 4) {
+                // 运动
+                
+            } elseif ($i == 5) {
+                // 紫外线强度
+
+            }
+        }
                 
         $newsTpl = "<xml>
 <ToUserName><![CDATA[%s]]></ToUserName>
@@ -214,9 +299,38 @@ $item_str</Articles>
 $item_str</Articles>
 </xml>";
 
-        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count($weather_data));
+        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count($weather_data)+2);
         return $result;
     }   
+
+    private function receiveImage($object)
+    {
+        $itemTpl = "<item>
+                    <Title><![CDATA[%s]]></Title>
+                    <Description><![CDATA[%s]]></Description>
+                    <PicUrl><![CDATA[%s]]></PicUrl>
+                    <Url><![CDATA[%s]]></Url>
+                    </item>";
+                    
+        $item_str = "";
+                        
+        $item_str .= sprintf($itemTpl, '我是至简生活，给我发图片是什么意思', '请输入正常的城市名',
+                                                 'http://7xqhcq.com1.z0.glb.clouddn.com/zjlife/CilEmlbI4_GAXd0TAADmniNS4gA215.jpg', 
+                                                 'https://weidian.com/s/1202372938?wfr=c&ifr=shopdetail');      
+        $newsTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[news]]></MsgType>
+            <Content><![CDATA[]]></Content>
+            <ArticleCount>%s</ArticleCount>
+            <Articles>
+            $item_str</Articles>
+            </xml>";
+
+        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count(1));
+        return $result;
+    }
     
     private function logger($log_content)
     {
